4d05e04a137b4d57ba138dd760c8e13b873bf8c2
Matthieu - Greep <42576124+GreepTheSheep@users.noreply.github.com>
2020-08-24 19:38:57 +0200

    Encore une inversion fixée..

--- cmds/cmds_index.js
+++ cmds/cmds_index.js
@@ -8,31 +8,31 @@ function cmds_index(message, client, prefix, cool, config, f){
 
     // -- Gifs
     const cry = require('./gifs/cry.js')
-    cry(client, message, prefix, cool, f)
+    cry(message, client, prefix, cool, f)
 
     const dance = require('./gifs/dance.js')
-    dance(client, message, prefix, cool, f)
+    dance(message, client, prefix, cool, f)
 
     const grimace = require('./gifs/grimace.js')
-    grimace(client, message, prefix, cool, f)
+    grimace(message, client, prefix, cool, f)
 
     const hey = require('./gifs/hey.js')
-    hey(client, message, prefix, cool, f)
+    hey(message, client, prefix, cool, f)
 
     const hug = require('./gifs/hug.js')
-    hug(client, message, prefix, cool, f)
+    hug(message, client, prefix, cool, f)
 
     const kiss = require('./gifs/kiss.js')
-    kiss(client, message, prefix, cool, f)
+    kiss(message, client, prefix, cool, f)
 
     const please = require('./gifs/please.js')
-    please(client, message, prefix, cool, f)
+    please(message, client, prefix, cool, f)
 
     const smile = require('./gifs/smile.js')
-    smile(client, message, prefix, cool, f)
+    smile(message, client, prefix, cool, f)
 
     const smug = require('./gifs/smug.js')
-    smug(client, message, prefix, cool, f)
+    smug(message, client, prefix, cool, f)
 
     // -- Staff
     const change = require('./staff/change.js')
