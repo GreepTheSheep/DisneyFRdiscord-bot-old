d42b0236ab1fbf09676f4e4caba86a457ae304a1
Matthieu - Greep <42576124+GreepTheSheep@users.noreply.github.com>
2020-08-25 13:11:19 +0200

    Update change.js

--- cmds/staff/change.js
+++ cmds/staff/change.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js')
 
 function change(message, client, prefix, cool, config){
-    if(message.content === "change"){
+    if(message.content == prefix + "change"){
         if(!config.staff.includes(message.author.id)) return
         client.channels.get("620292700260007970").setName("💫┃𝗗isney-𝗙𝗥-➳" + (message.guild.memberCount - message.guild.members.filter(e => e.user.client).size))
         message.react("✅")
