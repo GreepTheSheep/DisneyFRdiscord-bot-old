d6f6919f4529779c7abff91e1d0a4a86f3222173
Matthieu - Greep <42576124+GreepTheSheep@users.noreply.github.com>
2020-08-27 19:02:06 +0200

    Update help-dm.js

--- dm/help-dm.js
+++ dm/help-dm.js
@@ -4,8 +4,10 @@ function helpDM(message, client,prefix, config, f){
     let embed = new Discord.RichEmbed
     embed.setColor('RANDOM')
     .setTitle(`Commandes de ${client.user.username} en message privé:`)
-    .addField('Modmail', '`/modmail` - Ouvre le menu modmail pour pouvoir contacter les mods')
-    if (!message.content.startsWith('help') || !message.content.startsWith(prefix + 'help')) embed.addField('Autres:', 'Vous savez qu\'on à caché des commandes un peu partout ?\n\nSinon niveau nouveautés c\'est vide')
+    .addField('Modmail', '`/modmail` - Ouvre le menu modmail pour pouvoir contacter les mods', true)
+    
+    if (message.content.startsWith('help') || message.content.startsWith(prefix + 'help')) embed.addField('Et sinon?', 'Vous savez qu\'on à caché des commandes un peu partout ?')
+    
     if (message.content.startsWith('help') || message.content.startsWith(prefix + 'help')) message.author.send(embed)
     else message.author.send('Je ne vois pas ce que t\'as mis... Je suis qu\'un bot et j\'ai pas de cerveau 😧', embed)
 }
