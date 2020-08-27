285511abad5bdb0c32af473464c2476e7c6cc7a9
Matthieu - Greep <42576124+GreepTheSheep@users.noreply.github.com>
2020-08-27 20:13:30 +0200

    ajout du texte

--- cmds/infos/modmail.js
+++ cmds/infos/modmail.js
@@ -5,7 +5,7 @@ function modmailInfo(message, client,prefix, config, f){
         let embed = new Discord.RichEmbed
         embed.setImage('https://cdn.discordapp.com/attachments/662735703284908067/748602330798358620/modmail_pascal.gif')
         embed.setTitle('Utilisation du modmail')
-        embed.setDescription('Le modmail est un outil permettant de contacter les admins et modérateurs du serveurs pour signaler quelqu\'un.\nPour l\'utiliser, il faut ouvrir un ticket en message privé à moi en tapant la même commande')
+        embed.setDescription('Le modmail est un outil permettant de contacter les admins et modérateurs du serveurs pour signaler quelqu\'un ou nous poser des questions ou pour faire une candidature ou pour une demande de partenariat.\n\n__Pour l\'utiliser, il faut ouvrir un ticket en message privé à moi en tapant la même commande__')
         embed.setColor('RANDOM')
 
         message.channel.send('Le modmail est toujours en cours de construction...', embed)
