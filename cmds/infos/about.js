1129cddd6ab362e684029d6cf8f656a6cda04195
Matthieu - Greep <42576124+GreepTheSheep@users.noreply.github.com>
2020-08-27 17:54:51 +0200

    Traduction

--- cmds/infos/about.js
+++ cmds/infos/about.js
@@ -11,7 +11,7 @@ async function about(message, client, prefix, config, f) {
             if (!discordjsver) var discordjsver = 'pas trouvé'
             var nodever = shell.exec('node -v', {silent:true}).stdout.replace('v','').replace('\n','')
             if (!nodever) var nodever = 'pas trouvé'
-            var sysuptime = shell.exec('uptime --pretty', {silent:true}).stdout.replace('up ','').replace('\n','')
+            var sysuptime = shell.exec('uptime --pretty', {silent:true}).stdout.replace('up ','').replace('\n','').replace('weeks', 'semaines').replace('days', 'jours').replace('hours', 'heures')
             if (!nodever) var sysuptime = 'pas trouvé'
 
             let totalSeconds = (client.uptime) / 1000;
@@ -25,9 +25,9 @@ async function about(message, client, prefix, config, f) {
             let aboutembed = new Discord.RichEmbed()
             aboutembed.setColor("#9C01C4")
             .setTitle('A propos de ' + client.user.username)
-            .addField('Temps en ligne:', `🤖: ${weeks} weeks, ${days} days, ${hours} hours, ${minutes} minutes\n💻: ${sysuptime}`)
+            .addField('Temps en ligne:', `🤖: ${weeks} semaines, ${days} jours, ${hours} heures, ${minutes} minutes\n💻: ${sysuptime}`)
             .addField("Cast:", '- <@302092279114629121> pour l\'idée\n- <@299560596976697344> pour avoir commencé à le faire\n- <@330030648456642562> pour avoir repris le developpement')
-            .addField('Infos avancées:', `Bibliothèque utilisée: [Discord.js](https://discord.js.org) (Version ${discordjsver})\nVersion du moteur Node.js ${nodever}\nSystème utilisée pour faire fonctionner ce bot: ${os.type}: ${os.release}\nMémoire utilisée ${Math.round(os.freemem() / 1024 / 1000)}/${Math.round(os.totalmem() / 1024 / 1000)} MB [${(Math.round(os.freemem() / 1024 / 1000) * 100 / Math.round(os.totalmem() / 1024 / 1000)).toFixed(0)}%] (${client.user.username} utilise ${Math.round(process.memoryUsage().rss / 1024 / 1000)} MB [${(Math.round(process.memoryUsage().rss / 1024 / 1000) * 100 / Math.round(os.totalmem() / 1024 / 1000)).toFixed(0)}%])`)
+            .addField('Infos avancées:', `Bibliothèque utilisée: [Discord.js](https://discord.js.org) (Version ${discordjsver})\nVersion du moteur Node.js: ${nodever}\nSystème utilisée pour faire fonctionner ce bot: ${os.type}: ${os.release}\nMémoire utilisée ${Math.round(os.freemem() / 1024 / 1000)}/${Math.round(os.totalmem() / 1024 / 1000)} MB [${(Math.round(os.freemem() / 1024 / 1000) * 100 / Math.round(os.totalmem() / 1024 / 1000)).toFixed(0)}%] (${client.user.username} utilise ${Math.round(process.memoryUsage().rss / 1024 / 1000)} MB [${(Math.round(process.memoryUsage().rss / 1024 / 1000) * 100 / Math.round(os.totalmem() / 1024 / 1000)).toFixed(0)}%])`)
             .setThumbnail(client.user.displayAvatarURL)
             .setFooter(client.user.username, client.user.displayAvatarURL)
             message.channel.send(aboutembed).then(m.delete())
