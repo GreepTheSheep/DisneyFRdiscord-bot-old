885e6c16e21c4f8010f98e2606708482e4c2061d
Matthieu - Greep <42576124+GreepTheSheep@users.noreply.github.com>
2020-08-28 15:44:54 +0200

    Retrait du menu help a chaque message

--- dm/help-dm.js
+++ dm/help-dm.js
@@ -4,15 +4,16 @@ const Discord = require('discord.js')
 const fs = require('fs')
 
 function helpDM(message, client,prefix, config, f){
-    let embed = new Discord.RichEmbed
-    embed.setColor('RANDOM')
-    .setTitle(`Commandes de ${client.user.username} en message privé:`)
-    .setDescription(fs.readFileSync('./data/textes/Commandes-MP.md', 'utf-8'))
-    
-    if (message.content.startsWith('help') || message.content.startsWith(prefix + 'help')) embed.addField('Et sinon?', 'Vous savez qu\'on à caché des commandes un peu partout ?')
-    
-    if (message.content.startsWith('help') || message.content.startsWith(prefix + 'help')) message.author.send(embed)
-    else message.author.send('Je ne vois pas ce que t\'as mis... Je suis qu\'un bot et j\'ai pas de cerveau 😧', embed)
+    if (message.content.startsWith('help') || message.content.startsWith(prefix + 'help') || message.content.startsWith('aide') || message.content.startsWith(prefix + 'aide') || message.content.startsWith('aled') || message.content.startsWith(prefix + 'aled')) {
+        let embed = new Discord.RichEmbed
+        embed.setColor('RANDOM')
+        .setTitle(`Commandes de ${client.user.username} en message privé:`)
+        .setDescription(fs.readFileSync('./data/textes/Commandes-MP.md', 'utf-8'))
+        
+        embed.addField('Et sinon?', 'Vous savez qu\'on à caché des commandes un peu partout ?')
+        
+        message.author.send(embed)
+    }
 }
 
 module.exports = helpDM
