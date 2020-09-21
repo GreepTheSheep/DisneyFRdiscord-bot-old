a58d6170fed63785105243e233af1b4c0495e945
Matthieu <89matt89.md@gmail.com>
2020-09-21 19:29:03 +0200

    Update modmail-close.js

--- cmds/staff/modmail-close.js
+++ cmds/staff/modmail-close.js
@@ -14,16 +14,14 @@ async function modmail_close(message, client, prefix, config, f){
                 await menu.react('❌')
 
                 const filter = (reaction, user) => {
-                    return emojis.includes(reaction.emoji.name) && user.id === message.author.id;
+                    return user.id === message.author.id;
                 };
 
                 menu.awaitReactions(filter, { max: 1, time: 120000, errors: ['time'] })
                 .then(collected => {
                     const reaction = collected.first();
                     
-                    if (reaction.emoji.name == '❌'){
-                        menu.delete()
-                    } else {
+                    if (reaction.emoji.name == '✔'){
                         menu.edit('<:Snap:661557175130521610>')
                         message.channel.fetchMessages.then(messages => {
                             fs.writeFileSync('./data/modmail/' + message.channel.name + '.txt', messages)
@@ -35,7 +33,8 @@ async function modmail_close(message, client, prefix, config, f){
                             
                             // }
                         })
-                        .catch(console.error);
+                    } else {
+                        menu.delete()
                     }		
                 })
                 .catch(collected => {
