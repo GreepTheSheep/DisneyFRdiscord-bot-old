d1e202c4e0c564a8fd4e24145f6e6ca72082810b
Matthieu <89matt89.md@gmail.com>
2020-09-21 18:04:05 +0200

    Création des autres tickets

--- dm/modmail/options/4-partner.js
+++ dm/modmail/options/4-partner.js
@@ -1,7 +1,26 @@
 const Discord = require('discord.js')
 
 function modmailPartner(message, client, prefix, config, f, reaction, menu, server){
-    message.author.send('On y bosse, déso')
+    server.createChannel('partener-' + message.author.username, {type: "text"}).then(async channel=>{
+        channel.overwritePermissions(server.roles.find(r => r.name == '@everyone'), {READ_MESSAGES: false})
+        channel.overwritePermissions(message.author.id, {READ_MESSAGES: true})
+        channel.overwritePermissions('600643775978799115', {READ_MESSAGES: true})
+
+        channel.overwritePermissions('330030648456642562', {READ_MESSAGES: true}) // Accees a Greep
+
+        let category = server.channels.find(c => c.id == '757559028661354536' && c.type == "category");
+
+        if (!category) console.error("La catégorie Modmail (757559028661354536) n'existe pas...")
+        channel.setParent(category.id);
+
+        channel.send('<:Dipper:673927256778997760> Bienvenue dans le modmail <@' + message.author.id + '>!\n- __Vous avez choisi la raison de faire une demande de partenaire__, le <@&600643775978799115> vous répondra !\n\n\`\`\`Staff: utilisez la commande ' + prefix + 'mmclose pour fermer le ticket\`\`\`')
+
+        let embed = new Discord.RichEmbed
+        embed.setColor('#12E74D')
+        .setTitle('✔ Modmail crée!')
+        .setDescription('Veuillez vous rendre sur <#' + channel.id + '>')
+        menu.edit(embed)
+    })
 }
 
 module.exports = modmailPartner
--- dm/modmail/options/5-other.js
+++ dm/modmail/options/5-other.js
@@ -1,7 +1,26 @@
 const Discord = require('discord.js')
 
 function modmailOther(message, client, prefix, config, f, reaction, menu, server){
-    message.author.send('On y bosse, déso')
+    server.createChannel('autre-' + message.author.username, {type: "text"}).then(async channel=>{
+        channel.overwritePermissions(server.roles.find(r => r.name == '@everyone'), {READ_MESSAGES: false})
+        channel.overwritePermissions(message.author.id, {READ_MESSAGES: true})
+        channel.overwritePermissions('600643775978799115', {READ_MESSAGES: true})
+
+        channel.overwritePermissions('330030648456642562', {READ_MESSAGES: true}) // Accees a Greep
+
+        let category = server.channels.find(c => c.id == '757559028661354536' && c.type == "category");
+
+        if (!category) console.error("La catégorie Modmail (757559028661354536) n'existe pas...")
+        channel.setParent(category.id);
+
+        channel.send('<:Dipper:673927256778997760> Bienvenue dans le modmail <@' + message.author.id + '>!\n- __Vous avez choisi une autre raison__, le staff vous répondra ! (le temps de réponse peut varier selon la disponibilité du staff)\n\n\`\`\`Staff: utilisez la commande ' + prefix + 'mmclose pour fermer le ticket\`\`\`')
+
+        let embed = new Discord.RichEmbed
+        embed.setColor('#12E74D')
+        .setTitle('✔ Modmail crée!')
+        .setDescription('Veuillez vous rendre sur <#' + channel.id + '>')
+        menu.edit(embed)
+    })
 }
 
 module.exports = modmailOther
