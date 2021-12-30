353d97937771ed20eb4e72eea02b04481401f013
lustitch <36079399+lustitch@users.noreply.github.com>
2021-12-30 21:45:14 +0100

    Create pub.js

---
+++ cmds/infos/pub.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js')
+
+const fs = require('fs')
+
+function help(message, client, prefix){
+
+    if (message.content.startsWith(prefix + "pub")){
+
+        let embed = new Discord.MessageEmbed()
+
+        embed.setColor("2E2EFE")
+
+        .setAuthor("Voici la publicité du serveur pour " + message.author.username + " :",client.user.displayAvatarURL())
+
+        .setDescription(fs.readFileSync('./data/textes/pub.md', 'utf-8'))
+
+        .setFooter(`A partager n'importe où sans modération !`)
+
+        message.channel.send(embed)
+
+    }
+
+}
+
+module.exports = help
